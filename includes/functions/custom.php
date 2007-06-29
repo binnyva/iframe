b6e19cf1c7f2496525048a9274f582fd66b04580
binnyva <binnyva@gmail.com>
2007-06-29 05:51:17 +0000

    git-svn-id: http://www.bin-co.com/php/scripts/iframe/code@10 11ef9090-4330-0410-97fb-800521c72467

--- includes/functions/custom.php
+++ includes/functions/custom.php
@@ -54,3 +54,61 @@ function showMessage($message, $url="?", $status="success",$id=0) {
 	}
 	exit;
 }
+
+/**
+ * Read the plugin folder and put all the plugins found there in the dropdown menu
+ */
+function loadPlugins() {
+	global $rel,$abs;
+	
+	$plugins = array();
+	// Open plugin directory, and proceed to read its contents
+	$dir = $rel.'plugins/';
+	$files = ls($dir);
+	foreach($files as $file) {
+		if($file == 'CVS' || $file == '.' || $file =='..' || $file =='api' || $file == '.svn'
+		|| filetype($dir . $file) != 'dir') continue;
+		else
+			$plugins[] = $file;
+	}
+	
+	//Show the dropdown menu only if there are plugins
+	if(count($plugins)) {
+		print '<li class="dropdown"><a href="'.$rel.'plugins/" class="plugin with-icon">Plugins</a>';
+
+		print "\n<ul class='menu-with-icon plugins'>\n";
+		foreach($plugins as $plug) {
+			print '<li><a href="'.$abs.'plugins/'.$plug.'">' . format($plug) . '</a></li>'."\n";
+		}
+		print '</ul></li>';
+	}
+}
+
+/**
+ * Try to find the project of the given task. Searches for the name of all the 
+ *  projects in the text. If any project is found, the function returns the id of that project
+ */
+function findProject($text) {
+	$text = strtolower($text);
+	global $projects;
+	
+	$project_id = 0;
+	$biggest_count = 0;
+	$words = preg_split("/[\s,\.]/",strtolower($text));
+	
+	$remove_words = array('the','in','a','i','of','if'); //Add more later.
+	$words = array_values(array_diff($words, $remove_words));
+	
+	foreach($projects as $id=>$name) {
+		$count = 0;
+		foreach($words as $word) {
+			if(empty($word)) continue;
+			$count += substr_count(strtolower($name),$word);
+		}
+		if($count > $biggest_count) {
+			$project_id = $id;
+			$biggest_count = $count;
+		}
+	}
+	return $project_id;
+}
