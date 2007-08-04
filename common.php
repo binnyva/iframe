7dea9db197a68dab9d9b657b9c3e8be97c9b1225
binnyva <binnyva@gmail.com>
2007-08-04 20:11:04 +0000

    git-svn-id: http://www.bin-co.com/php/scripts/iframe/code@16 11ef9090-4330-0410-97fb-800521c72467

--- common.php
+++ common.php
@@ -1,16 +1,17 @@
 <?php
 $rel = findRelation();
 include($rel . "configuration.php");
-include($rel . "includes/classes/Sql.php");
-include($rel . "includes/config.php");
-include($rel . "includes/classes/Template.php");
-include($rel . "includes/functions.php");
+
+$this_folder = dirname(__FILE__) . '/';
+include($this_folder . "includes/classes/Sql.php");
+include($this_folder . "includes/config.php");
+include($this_folder . "includes/functions.php");
+include($this_folder . "includes/classes/MVC.php");
 
 $PARAM = unescapeQuery();
 $QUERY = escapeQuery($PARAM,true);
 if(!isset($QUERY['error']))	 $QUERY['error'] = '';
 if(!isset($QUERY['success']))$QUERY['success'] = '';
-
 
 ////////////////////////////////////// Functions /////////////////////////////////////////////////
 //Find the relation between the page we are in and the root folder.
