4cbb5f11748d6edbb94bf9a53b1b58fc81a11927
binnyva <binnyva@gmail.com>
2008-05-31 19:20:46 +0000

    Resolving a conflict
    git-svn-id: http://www.bin-co.com/php/scripts/iframe/code@48 11ef9090-4330-0410-97fb-800521c72467

--- templates/layout/page.php
+++ templates/layout/page.php
@@ -2,7 +2,7 @@
 <html><head>
 <title><?=$title?></title>
 <link href="<?=$abs?>css/style.css" rel="stylesheet" type="text/css" />
-<script src="/iframe/js/jsl.js" type="text/javascript"></script>
+<script src="<?=$abs?>js/jsl.js" type="text/javascript"></script>
 <script src="<?=$abs?>js/application.js" type="text/javascript"></script>
 <?=$includes?>
 </head>
