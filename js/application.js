7f9a5f4c07c902684f5496a76e13e070cdeb03b9
binnyva <binnyva@gmail.com>
2008-10-24 13:54:45 +0000

    JS Files moved around a bit
    git-svn-id: http://www.bin-co.com/php/scripts/iframe/code@64 11ef9090-4330-0410-97fb-800521c72467

--- js/application.js
+++ js/application.js
@@ -7,7 +7,27 @@ function stripSlashes(text) {
 	if(!text) return "";
 	return text.replace(/\\([\'\"])/,"$1");
 }
+
+
+function ajaxError() {
+	alert("Error communicating with server. Please try again");
+}
+function loading() {
+	$("loading").show();
+}
+function loaded() {
+	$("loading").hide();
+}
+
+
 function siteInit() {
+	$("a.confirm").click(function(e) { //If a link has a confirm class, confrm the action
+		var action = (this.title) ? this.title : "do this";
+		action = action.substr(0,1).toLowerCase() + action.substr(1); //Lowercase the first char.
+		
+		if(!confirm("Are you sure you want to " + action + "?")) JSL.event(e).stop();
+	});
+
 	if(window.init && typeof window.init == "function") init(); //If there is a function called init(), call it on load
 }
-window.onload=siteInit;
+$(window).load(siteInit);
