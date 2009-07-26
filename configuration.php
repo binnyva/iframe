5819a05dfbec42fe0b28b54c540f69718d656d10
binnyva <binnyva@gmail.com>
2009-07-26 18:58:31 +0000

    Bare minimum configuration file
    git-svn-id: http://www.bin-co.com/php/scripts/iframe/code@73 11ef9090-4330-0410-97fb-800521c72467

--- configuration.php
+++ configuration.php
@@ -1,12 +1,5 @@
 <?php
 //Configuration file for iFrame
-$system_installed = true;
 $config = array(
 	'site_title'	=>	'iFrame',
-	'db_host'		=>	'localhost',
-	'db_user'		=>	'root',
-	'db_password'	=>	'',
-	'db_database'	=>	'Project_Nexty',
-	'url'			=>	'http://localhost/iframe/',
-	'absolute_path'	=>	'/iframe/'
 );
